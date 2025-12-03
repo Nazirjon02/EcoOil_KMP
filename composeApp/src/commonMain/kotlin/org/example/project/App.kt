83a27afee9c5bef036395ce0fb671042a98665ce
Bek <bekhzod24@bk.ru>
2025-12-03 18:04:21 +0500

    test in macbook

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -32,7 +32,7 @@ fun App() {
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Button(onClick = { showContent = !showContent }) {
-                Text("Click !")
+                Text("Click  test!")
             }
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
